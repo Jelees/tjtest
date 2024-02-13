e4b904bfc2e247ad9d777957ba315a177700a401
ljj <whdwo798@naver.com>
2024-02-13 10:44:34 +0900

    java_02.13

---
+++ src/ex01/Collection01.java
@@ -0,0 +1,30 @@
+package ex01;
+
+import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Collection01 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        arrList.add(40);
+        arrList.add(30);
+        arrList.add(20);
+        arrList.add(10);
+
+        for (int i = 0; i < arrList.size(); i++) {
+            System.out.print(arrList.get(i)+ " ");
+        }
+        arrList.remove(1);
+
+        for (int e : arrList){
+            System.out.print(e + " ");
+        }
+
+        System.out.println();
+
+        Collections.sort(arrList);
+        Iterator<Integer> iter = arrList.iterator();
+
+    }
+}
